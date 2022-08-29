a2987ce5ee2299d96d40cd4d6c761c16abfd6005
Malgorzata Stano <stano.malgorzata@gmail.com>
2022-08-29 08:20:11 +0200

    added a footer with Github repo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import "./App.css";
 
 export default function App() {
-  return (
-    <div className="App">
-      Weather App
-    </div>
-  );
+	return (
+		<div className="App">
+			Weather App
+			<footer>
+        This project was coded by <a href="https://github.com/gosiast"> Malgorzata Stano </a>
+        and is 
+				<a href="https://github.com/gosiast/final-weather-app" target="_blank">
+					{" "}
+					open sourced{" "}
+				</a>{" "}
+			</footer>
+		</div>
+	);
 }
-
