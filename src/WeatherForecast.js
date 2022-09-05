b463f4e5c1e1725b07bd85c9079e030eb4306a45
Malgorzata Stano <stano.malgorzata@gmail.com>
2022-09-05 09:23:51 +0200

    fixed bug

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,7 +17,6 @@ export default function WeatherForecast(props) {
 		setLoaded(false);
 	}, [props.coordinates]);
 
-	
 	function handleResponse(response) {
 		console.log(response.data);
 		setForecast(response.data.daily);
@@ -25,38 +24,35 @@ export default function WeatherForecast(props) {
 		// it won't display the daily forecast
 	}
 
-	if (loaded) {
-		return (
-			<div className="WeatherForecast">
-				<div className="row">
-					<hr />
-					<div className="col">
-						<WeatherForecastDay data={forecast[0]} />
-					</div>
-					<div className="col">
-						<WeatherForecastDay data={forecast[1]} />
-					</div>
-					<div className="col">
-						<WeatherForecastDay data={forecast[2]} />
-					</div>
-					<div className="col">
-						<WeatherForecastDay data={forecast[3]} />
-					</div>
-					<div className="col">
-						<WeatherForecastDay data={forecast[4]} />
-					</div>
-				</div>
-			</div>
-		);
-
-		// another option to display multiple days was to use the LOOP
-	} else {
-		let apiKey = "1e7e4fc41dea029f49d7f03e46e28df4";
+	function load() {
+		let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
 		let longitude = props.coordinates.lon;
 		let latitude = props.coordinates.lat;
 		let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
 		axios.get(apiUrl).then(handleResponse);
+	}
+
+	if (loaded) {
+		return (
+			<div className="WeatherForecast">
+				<div className="row">
+					{forecast.map(function(dailyForecast, index) {
+						if (index < 5) {
+							return (
+								<div className="col" key={index}>
+									<WeatherForecastDay data={dailyForecast} />
+								</div>
+							);
+						} else {
+							return null;
+						}
+					})}
+				</div>
+			</div>
+		);
+	} else {
+		load();
 
 		return null;
 	}
